54878c4b37306e448f4af4b16f347ec2539ca8fe
ViniciusKirsten <vinicius.k.mendonca@outlook.com>
2022-12-05 19:49:50 -0300

    Arquivos de estudo 05/12/2022
    Pilares de orientação a objetos.
    Enum.

---
+++ src/oo/heranca/Direcao.java
@@ -0,0 +1,15 @@
+package oo.heranca;
+
+public enum Direcao {
+
+	/*
+	 * Vai ser a minha enumeração.
+	 * 
+	 * Vamos usar esse tipo de caso quando tivermos uma quantidade
+	 * finita que sabemos o começo e o fim, como dias da semana
+	 * coordenadas.
+	 */
+	
+	//nao representa uma string nem um valor numerico 
+	NORTE, LESTE, SUL, OESTE;
+}
